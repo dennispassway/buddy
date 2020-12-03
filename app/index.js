3c75404225ae659f94b714066bdc39731cef70a3
Dennis Passway <dennispassway@gmail.com>
2020-12-03 15:26:15 +0100

    Enable all functionality

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
-// const { ACTIONS } = require("./constants");
+const { ACTIONS } = require("./constants");
 const { App } = require("@slack/bolt");
 const { createReceiver } = require("./createReceiver");
-// const { handleAction } = require("./handleAction");
-// const { handleAppHome } = require("./handleAppHome");
-// const { handleCommand } = require("./handleCommand");
-// const { handleMessage } = require("./handleMessage");
+const { handleAction } = require("./handleAction");
+const { handleAppHome } = require("./handleAppHome");
+const { handleCommand } = require("./handleCommand");
+const { handleMessage } = require("./handleMessage");
 
 const receiver = createReceiver(process.env.SLACK_SIGNING_SECRET);
 
@@ -15,10 +15,10 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
 });
 
-// app.event("app_home_opened", handleAppHome);
-// app.event("message", handleMessage);
-// app.command("/buddy", handleCommand);
-// ACTIONS.map((action) => app.action(action, handleAction));
+app.event("app_home_opened", handleAppHome);
+app.event("message", handleMessage);
+app.command("/buddy", handleCommand);
+ACTIONS.map((action) => app.action(action, handleAction));
 app.error((error) => console.error(error)); /* @TODO: sentry */
 
 module.exports = { app, receiver };
